2cb5eaafa1f33ae1df36ebcc442147e8c6f57eef
Buddy Agyin <buddyagyin@Buddys-MacBook-Pro.local>
2019-09-25 01:31:59 -0600

    add date picker

--- src/components/ImageData.js
+++ src/components/ImageData.js
@@ -1,26 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import ImageCard from './ImageCard.js';
 import axios from 'axios';
-import MockData from './MockData.js';
+// import MockData from './MockData.js';
+import Form from './Form.js'
 
 export default function ImageData () {
+
+    const currentDate = new Date();
+    const formattedDate = currentDate.getFullYear() + "-" + (currentDate.getMonth() + 1) + "-" + currentDate.getDate();
     const [image, setImage] = useState([]);
+    const [query, setQuery] = useState(formattedDate);
 
     useEffect(() => {
         async function fetchData() {
             try {
-                const imageData = await axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
+                const imageData = await axios.get('https://api.nasa.gov/planetary/apod?api_key=uPC11JidhuaFX8k6hPqzgCR0WzZYkpn6pBAFQCAf&date=' + query);
                 setImage(imageData.data);
             }catch(err) {
                 console.log(err);
             }
         }
         fetchData();
-    }, [])
+
+        // setImage(MockData);
+    }, [query])
+
+    const changeDate = date => {
+        setQuery(date.target.value);
+    }
+
+
 
     return (
         <div>
-            <ImageCard title= {image.title} src = {image.hdurl} description = {image.explanation} alt = {image.title} />
+            <Form submit = {changeDate} />
+            <ImageCard title= {image.title} src = {image.url} description = {image.explanation} alt = {image.title} />
         </div>
     );
 }
